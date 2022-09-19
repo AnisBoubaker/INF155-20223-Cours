cebe70e9aca9b2d447cce770a295681f5397791f
Anis Boubaker <anis@boubaker.ca>
2022-09-19 13:56:58 -0400

    Gr3 - C03

---
+++ GR03/C03/SommeEntiers/main.c
@@ -0,0 +1,16 @@
+/*
+Écrire un programme qui saisit (demande à l'usager de saisir) un entier n. 
+
+Votre programme doit faire la somme des valeurs entières entre 1 et n. 
+
+Exemple 1: 
+------------
+Veuillez saisir la valeur de n: 5
+La somme des entiers entre 1 et 5 est: 15
+
+Exemple 2:
+------------
+Veuillez saisir la valeur de n: 100
+La somme des entiers entre 1 et 100 est: 5050
+
+*/
