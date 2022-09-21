595a9910ee7b14a948dcf1b6ac997bfe326139a4
Anis Boubaker <anis@boubaker.ca>
2022-09-21 10:27:15 -0400

    Gr3 - C03

--- GR03/C03/Boucle_for/main.c
+++ GR03/C03/Boucle_for/main.c
@@ -5,3 +5,26 @@ une valeur saisie par l'usager.
 Dans cet exercice, vous devez utiliser une boucle for. 
 */
 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int n; 
+	int resultat; 
+
+	printf("Saisir la valeur de n: ");
+	scanf("%d", &n);
+
+	resultat = 1;
+	for (int compteur = 1; compteur <= n; compteur++)
+	{
+		resultat = resultat * compteur;
+	}
+
+	printf("Le produit des entiers entre 1 et %d est: %d\n", n, resultat);
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
--- GR03/C03/SommeEntiers/main.c
+++ GR03/C03/SommeEntiers/main.c
@@ -14,3 +14,37 @@ Veuillez saisir la valeur de n: 100
 La somme des entiers entre 1 et 100 est: 5050
 
 */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int n;
+	int compteur; 
+	int resultat; 
+
+	printf("Veuillez saisir la valeur de n: ");
+	scanf("%d", &n);
+
+
+	resultat = 0; 
+	for (compteur = 1; compteur <= n; compteur++)
+	{
+		resultat = resultat + compteur;
+	}
+
+	/*compteur = 1; 
+	resultat = 0; 
+	while (compteur <= n)
+	{
+		resultat = resultat + compteur;
+		compteur++;
+	}*/
+
+	printf("La somme des valeurs entre 1 et %d est: %d\n", n, resultat);
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
