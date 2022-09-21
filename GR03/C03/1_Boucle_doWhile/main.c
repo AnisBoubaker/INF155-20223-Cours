1617871dd105bd5a2c0d330dcf8ea2c00b58a45f
Anis Boubaker <anis@boubaker.ca>
2022-09-21 08:43:19 -0400

    Gr3 - C03

---
+++ GR03/C03/1_Boucle_doWhile/main.c
@@ -0,0 +1,32 @@
+/*
+Écrire un programme qui demande à l'usager de saisir un nombre positif. 
+
+Si le nombre saisi n'est pas positif, le programme lui redemande jusqu'à 
+obtention d'un nombre positif. 
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int saisie; //Valeur saisie par l'usager
+
+	//On saisit une première fois.
+	printf("Veuillez saisir un nombre positif: ");
+	scanf("%d", &saisie);
+
+	//On répète tant que la valeur saisie n'est pas bonne
+	while (saisie < 0)
+	{
+		printf("Veuillez saisir un nombre positif: ");
+		scanf("%d", &saisie);
+	}
+
+	//Si on sort de la boucle, c'est que la valeur saisie est bonne, on dit merci.
+	printf("Merci!\n");
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
---
+++ GR03/C03/Boucle_for/main.c
@@ -0,0 +1,7 @@
+/*
+Écrire un programme qui fait le produit des entiers entre 1 et n, n étant
+une valeur saisie par l'usager. 
+
+Dans cet exercice, vous devez utiliser une boucle for. 
+*/
+
