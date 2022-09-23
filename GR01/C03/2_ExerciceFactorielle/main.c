79038da1c26ee64126997b917430f2dcef7f24bb
Anis Boubaker <anis@boubaker.ca>
2022-09-23 09:37:59 -0400

    Gr3 - C03

---
+++ GR01/C03/2_ExerciceFactorielle/main.c
@@ -0,0 +1,10 @@
+/*
+Écrire un programme qui calcule la factorielle d'un nombre n saisi par l'usager.
+factorielle de n: 1 * 2 * 3 * 4 * 5 * ... * n
+factorielle de 5: 1 * 2  * 3 * 4 * 5 = 120
+Vous devez utiliser la boucle la plus adaptée.
+Exemple:
+--------
+Saisir la valeur de n: 5
+La factorielle de 5 est: 120
+*/
---
+++ GR01/C03/3_SousProgrammes/main.c
@@ -0,0 +1,4 @@
+/*
+Écrire un programme qui calcule le nombre de combinaisons possibles
+en tirant k boules à partir d'une urne contenant n boules.
+*/
