210f9cc7a3d426abf4b379a93c0fdc62eafe22be
Anis Boubaker <anis@boubaker.ca>
2022-10-12 08:50:34 -0400

    Gr03 - C06

---
+++ GR03/C06/0_Exercice_Tableaux/main.c
@@ -0,0 +1,26 @@
+/*
+Écrire une fonction qui identifie la position (indice) 
+d'une valeur 'val' dans un tableau tab d'entiers.
+
+La fonction doit retourner une valeur vraie si la valeur `val` est
+dans le tableau ou fausse sinon. De plus, la position doit être fournie
+par référence (pointeur).
+
+Si la valeur se trouve plus d'une fois dans le tableau, la fonction
+doit retourner la position de la première occurence.
+
+Si la valeur `val` ne se trouve pas dans le tableau, retourner 
+-1 comme position (pour indiquer qu'elle n'y est pas)
+
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+
+}
