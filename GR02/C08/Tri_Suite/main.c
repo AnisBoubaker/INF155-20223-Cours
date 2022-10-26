46ce767abc03072f2d700e83f9a5f0dd66218c9e
Anis Boubaker <anis@boubaker.ca>
2022-10-26 14:56:04 -0400

    Gr02 - C08

--- GR02/C08/Tri_Suite/main.c
+++ GR02/C08/Tri_Suite/main.c
@@ -6,11 +6,16 @@
 void afficher_tab(int tab[], int taille);
 void tri_insertion(int tab[], int taille);
 void tri_selection(int tab[], int taille);
+double mediane_tab(const int tab[], int taille);
+
+#define TAILLE_MAX 100
 
 int main(void)
 {
-	int mon_tab1[100] = { 44, 10, 20, 55, 10, 24, 35 };
-	int mon_tab2[100] = { 44, 10, 20, 55, 10, 24, 35 };
+	int mon_tab1[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35 };
+	int mon_tab2[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35 };
+	int mon_tab3[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35 };
+	int mon_tab4[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35, 11 };
 
 	printf("Avant le tri: \n");
 	afficher_tab(mon_tab1, 7);
@@ -24,6 +29,16 @@ int main(void)
 
 	printf("Apres le tri par selection: \n");
 	afficher_tab(mon_tab2, 7);
+
+	printf("La mediane du tableau tab3: %.2lf\n", mediane_tab(mon_tab3, 7));
+
+	printf("La mediane du tableau tab4: %.2lf\n", mediane_tab(mon_tab4, 8));
+
+	tri_selection(mon_tab4, 8);
+
+	printf("tab4 trie: \n");
+	afficher_tab(mon_tab4, 8);
+
 
 	system("pause");
 	return EXIT_SUCCESS;
@@ -89,9 +104,30 @@ void afficher_tab(int tab[], int taille)
 /*
 Écrire la fonction mediane_tab qui calcule la médiane d'un ensemble d'entiers 
 stockés dans un tableau. 
+NOTE: Le tableau reçu n'est pas forcément trié!!
 */
 
 double mediane_tab(const int tab[], int taille)
 {
+	int copie[TAILLE_MAX];
+	double mediane; 
 
+	//Créer une copie du tableau
+	for (int i = 0; i < taille; i++)
+	{
+		copie[i] = tab[i];
+	}
+
+	tri_selection(copie, taille);
+
+
+	if (taille % 2 != 0) //La taille est impaire
+	{
+		mediane = copie[taille / 2];
+	}
+	else
+	{
+		mediane = (copie[taille / 2] + copie[(taille / 2) - 1]) / 2.0;
+	}
+	return mediane; 
 }
