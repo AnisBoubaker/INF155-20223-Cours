4c2d01a8d56b087ae6f4764544dea2ca355e4c90
Anis Boubaker <anis@boubaker.ca>
2022-10-28 10:47:03 -0400

    Gr01 C08

---
+++ GR01/C08/Strings/main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE_MAX 100
+
+
+int main(void)
+{
+	//char salutation[8] = { 'B', 'o', 'n', 'j', 'o', 'u', 'r' };
+	char salutation[8] = "Bonjour";
+
+	printf("%s\n", salutation);
+
+
+
+
+
+	system("pause");
+	return 0;
+}
--- GR01/C08/Tri_suite/main.c
+++ GR01/C08/Tri_suite/main.c
@@ -2,15 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAILLE_MAX 100
+
+
 void afficher_tab(int tab[], int taille);
 void tri_insertion(int tab[], int taille);
 void tri_selection(int tab[], int taille);
+double mediane_tab(const int tab[], int taille);
+
 
 int main(void)
 {
-	int mon_tableau1[] = { 44, 10, 20, 55, 10, 24, 35 };
-	int mon_tableau2[] = { 44, 10, 20, 55, 10, 24, 35 };
-	int mon_tableau3[] = { 44, 10, 20, 55, 10, 24, 35 };
+	int mon_tableau1[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35 };
+	int mon_tableau2[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35 };
+	int mon_tableau3[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35 };
+	int mon_tableau4[TAILLE_MAX] = { 44, 10, 20, 55, 10, 24, 35, 17 };
 
 	printf("Avant le tri: \n");
 	afficher_tab(mon_tableau1, 7);
@@ -23,6 +29,13 @@ int main(void)
 	printf("Apres le tri selection: \n");
 	tri_selection(mon_tableau2, 7);
 	afficher_tab(mon_tableau2, 7);
+
+	printf("La mediane de tab3: %.2lf\n", mediane_tab(mon_tableau3, 7));
+
+	printf("La mediane de tab4: %.2lf\n", mediane_tab(mon_tableau4, 8));
+
+	tri_insertion(mon_tableau4, 8);
+	afficher_tab(mon_tableau4, 8);
 
 	system("pause");
 	return 0;
@@ -75,4 +88,37 @@ void tri_selection(int tab[], int taille)
 			tab[i] = copie; 
 		}
 	}
+}
+
+
+/*
+
+Écrire la fonction mediane_tab qui calcule la médiane
+d'un tableau d'entiers. 
+
+double mediane_tab(const int tab[], int taille);
+
+*/
+double mediane_tab(const int tab[], int taille)
+{
+	int copie[TAILLE_MAX];
+	double mediane; 
+
+	//On crée une copie du tableau pour pouvoir le trier (car const)
+	for (int i = 0; i < taille; i++)
+	{
+		copie[i] = tab[i];
+	}
+
+	tri_selection(copie, taille);
+
+	if (taille % 2 != 0)
+	{
+		mediane = copie[taille / 2];
+	}
+	else
+	{
+		mediane = (copie[taille / 2] + copie[taille / 2 - 1]) / 2.0;
+	}
+	return mediane;
 }
