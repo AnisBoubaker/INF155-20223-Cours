00ca9064999f3845cf46c81e585aee9836f6c63e
Anis Boubaker <anis@boubaker.ca>
2022-11-02 11:38:15 -0400

    Gr03 C09

--- GR03/C09/2_Structures/main.c
+++ GR03/C09/2_Structures/main.c
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct etudiant 
+/*
+- On définit le type struct etudiant (qui contient le nom, prenom, tpq, etc.)
+- Dans la même opération on renomme struct etudiant en t_etudiant
+*/
+typedef struct etudiant 
 {
 	char nom[100]; 
 	char prenom[100];
@@ -12,13 +16,20 @@ struct etudiant
 	double tp2; 
 	double intra;
 	double finale; 
-};
+} t_etudiant;
 
+//typedef struct etudiant t_etudiant; 
+
+
+
+//Déclarations des fonctions: 
+void etudiant_afficher(t_etudiant* et);
 
 int main(void)
 {
-
-	struct etudiant etudiant0; 
+	//Eq.: struct etudiant etudiant0
+	t_etudiant etudiant0; 
+	t_etudiant etudiant1;
 
 	strcpy(etudiant0.nom,  "Valjean");
 	strcpy(etudiant0.prenom, "Jean");
@@ -28,16 +39,37 @@ int main(void)
 	etudiant0.intra = 80;
 	etudiant0.finale = 85;
 
-	printf("Fiche de l'etudiant-e: \n");
+	/*printf("Fiche de l'etudiant-e: \n");
 	printf("Nom: %s\n", etudiant0.nom);
 	printf("Prenom: %s\n", etudiant0.prenom);
 	printf("Code Permanent: %s\n", etudiant0.code_perm);
 	printf("TP1: %.2lf\n", etudiant0.tp1);
 	printf("TP2: %.2lf\n", etudiant0.tp2);
 	printf("Intra: %.2lf\n", etudiant0.intra);
-	printf("Final: %.2lf\n", etudiant0.finale);
+	printf("Final: %.2lf\n", etudiant0.finale);*/
+
+	printf("La taille en octets d'un etudiant: %d\n", sizeof(t_etudiant));
+
+
+	etudiant_afficher(&etudiant0);
+
+	printf("La note de l'examen final de etudiant0: %.2lf\n", etudiant0.finale);
 
 
 	system("pause");
 	return EXIT_SUCCESS;
+}
+
+void etudiant_afficher(const t_etudiant* et)
+{
+	printf("Fiche de l'etudiant-e: \n");
+	printf("Nom: %s\n", et->nom);
+	printf("Prenom: %s\n", et->prenom);
+	printf("Code Permanent: %s\n", et->code_perm);
+	printf("TP1: %.2lf\n", et->tp1);
+	printf("TP2: %.2lf\n", (*et).tp2);
+	printf("Intra: %.2lf\n", (*et).intra);
+	printf("Final: %.2lf\n", (*et).finale);
+	//Plus possible car et est const
+	//(*et).finale = 0;
 }
