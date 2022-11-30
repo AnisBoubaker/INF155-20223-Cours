0b9ee223a82d4a92955ff81f6d71fe52f59652cd
Anis Boubaker <anis@boubaker.ca>
2022-11-30 17:12:18 -0500

    Gr2-C12

--- GR02/C12/ProblemeRevision/mod_capteur.c
+++ GR02/C12/ProblemeRevision/mod_capteur.c
@@ -1,6 +1,6 @@
 #include "mod_capteur.h"
 
-t_capteur* capteur_init(char* ville, double lat, double longi)
+t_capteur* capteur_init(char* ville, double lat, double longi, int nb_jours_max)
 {
 	t_capteur* nouv_capteur; 
 
@@ -16,6 +16,13 @@ t_capteur* capteur_init(char* ville, double lat, double longi)
 		free(nouv_capteur);
 		exit(EXIT_FAILURE);
 	}
+
+	/*
+	ALLOUER LE TAB 2D DES T_MESURE
+	Le nombre de lignes est défini par int nb_jours_max
+	Le nombre de colonnes est défini par NB_MESURES_PAR_JOUR
+	*/
+
 
 	nouv_capteur->latitude = lat;
 	nouv_capteur->longitude = longi;
